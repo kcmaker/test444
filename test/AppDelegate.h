b98ab3b40d98412444ec4d262e1342c311a0ff12
kcmaker <kc@kcmakerdeMac-mini.local>
2016-06-24 11:49:10 +0800

    test xcode

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by kcmaker on 16/6/24.
+//  Copyright © 2016年 kcmaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by kcmaker on 16/6/24.
+//  Copyright © 2016年 kcmaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
